c84c25949d4bae0a20684cfe615a7336220dd146
cathrin-11 <cathrin1103@gmail.com>
2026-08-20 14:57:16 +0530

    feat: enable CORS middleware in Express server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import { setupSwagger } from "./swagger";
 import { config } from "./config";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 setupSwagger(app);
+
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("Task API is running!");
